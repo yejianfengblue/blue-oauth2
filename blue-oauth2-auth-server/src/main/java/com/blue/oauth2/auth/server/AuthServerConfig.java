1a791574ebc172fc9a25a4c758a7933eb3ea8245
Jian Feng Ye <SPIJFY@cathaypacific.com>
2019-05-24 15:00:48 +0800

    Add a welcome page to give use to click LOGIN and then if login successfully display the user info

--- blue-oauth2-auth-server/src/main/java/com/blue/oauth2/auth/server/AuthServerConfig.java
+++ blue-oauth2-auth-server/src/main/java/com/blue/oauth2/auth/server/AuthServerConfig.java
@@ -1,10 +1,9 @@
 package com.blue.oauth2.auth.server;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableOAuth2Sso
+
 public class AuthServerConfig {
 
 }
